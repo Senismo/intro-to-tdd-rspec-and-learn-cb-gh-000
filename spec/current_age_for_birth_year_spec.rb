c28acb681a2a583b72575cedb3e02adeea13fb09
Salak Sen <benismail31@gmail.com>
2018-04-01 17:22:12 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,7 +3,9 @@ require_relative '../current_age_for_birth_year.rb'
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
-
-    expect(age_of_person).to eq(19)
+    year_of_birth = 1984
+    current_year = Time.now.year
+    expected_age = current_year - year_of_birth
+    expect(age_of_person).to eq(expected_age)
   end
 end
